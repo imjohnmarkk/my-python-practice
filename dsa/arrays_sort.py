e47da48c4bcc7a7d3bfdf980e6bd12c75bd718fd
John Mark <john.mark.devv@gmail.com>
2024-12-05 12:46:40 +0800

    Adding Bubble sort

--- dsa/arrays_sort.py
+++ dsa/arrays_sort.py
@@ -1,3 +1,20 @@
+#bubble sort
+def bubble_sort(arr):
+    
+	n = len(arr)
+    
+	for i in range(n-1):
+		swapped = False
+		for j in range(n-i-1):
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = True
+                
+		if not swapped:
+			break
+   
+	print(f"Sorted array: {arr}")  
+    
 #selection sort
 def selection_sort(arr):
     n = len(arr)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import dsa.arrays_sort as arysrt
 from dsa.test_var import arr1
 
-arysrt.selection_sort(arr1)
+arysrt.bubble_sort(arr1)
